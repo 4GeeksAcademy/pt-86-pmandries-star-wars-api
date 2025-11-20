a8ec1e10e21c413d5bf2a6874215ef10d81f447e
pmandries <pandries@aol.com>
2025-11-20 02:14:14 +0000

    basic shell

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,33 @@
-import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import React, { useState, useEffect } from 'react';
+import { Card } from "../components/Card.jsx"
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+  const {store, dispatch} =useGlobalReducer();
+  const [people, setPeople] = useState([]);
+
+  useEffect ( () => {
+	getPeople();
+  }, []);
+
+//   useEffect(() => {
+// 	console.log(people);
+//   }, [people]);
+
+  const getPeople = async() => {
+	const response = await fetch("https://www.swapi.tech/api/people");
+	const data = await response.json();
+	setPeople(data.results);
+  }
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+			<div className = "d-flex col-10 overflow-auto mt-5 mx-auto">
+				{people?.map((person, index) => {
+					return <Card key = {index} name = {person.name} />
+				})}
+			</div>	
 		</div>
 	);
 }; 
